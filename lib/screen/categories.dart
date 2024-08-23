8e69f3c51e12c51d725fdf721ac83a0b6a90685f
Vishakh Nair <vishakh.22bce7720@vitapstudent.ac.in>
2024-08-23 14:38:36 +0530

    Add files via upload

---
+++ lib/screen/categories.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/model/categories.dart';
+import 'package:meal_app/screen/meals.dart';
+import 'package:meal_app/widget/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectedCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) =>
+            MealsScreen(meals: filteredMeals, title: category.title),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("pick ur Screen"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectedCategory: () {
+                _selectedCategory(context, category);
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
